c8cebe54db6de41e5a3d799d2b69cb2779efeba1
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-08-13 03:00:42 +0900

    Initial Commit

---
+++ POSFront/POSFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  POSFrontApp.swift
+//  POSFront
+//
+//  Created by Owner on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct POSFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
